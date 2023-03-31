555154d44c36d3b5c6a880bc498c1e536618d647
okafordizzy <okaforchiedozie69@gmail.com>
2023-03-30 23:19:06 -0700

    New Commit

--- 0x04-TypeScript/task_0/js/main.js
+++ 0x04-TypeScript/task_0/js/main.js
@@ -1,48 +1,32 @@
-interface Student {
-    firstName: string;
-    lastName: string;
-    age: number;
-    location: string;
-}
-
-const student1: Student = {
+var student1 = {
     firstName: 'firdaus',
     lastName: 'salim',
     age: 21,
-    location: 'mombasa',
+    location: 'mombasa'
 };
-const student2: Student = {
+var student2 = {
     firstName: 'beta',
     lastName: 'scribbles',
     age: 25,
-    location: 'kenya',
+    location: 'kenya'
 };
-
-const studentsList: Student[] = [student1, student2];
-
-const table = document.createElement('table');
-const tbody = document.createElement('tbody');
-
+var studentsList = [student1, student2];
+var table = document.createElement('table');
+var tbody = document.createElement('tbody');
 table.style.background = "lightgray";
 table.appendChild(tbody);
-
-studentsList.forEach((student: Student): void => {
-    const row = document.createElement('tr');
-
-    const nameCell = document.createElement('td');
-    const locationCell = document.createElement('td');
-
+studentsList.forEach(function (student) {
+    var row = document.createElement('tr');
+    var nameCell = document.createElement('td');
+    var locationCell = document.createElement('td');
     nameCell.textContent = student.firstName;
     locationCell.textContent = student.location;
-
     nameCell.style.border = "2px solid white";
     locationCell.style.border = "2px solid white";
     nameCell.style.padding = "4px";
     locationCell.style.padding = "4px";
-
     row.appendChild(nameCell);
     row.appendChild(locationCell);
     tbody.appendChild(row);
 });
-
 document.body.appendChild(table);
